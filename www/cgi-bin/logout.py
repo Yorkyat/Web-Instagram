0975fc06f432a7c201ab1e6dd6848527dc7708f6
Yorkyat <s1155062584@cuhk.edu.hk>
2018-02-11 06:35:33 +0800

    Add: logout function

---
+++ www/cgi-bin/logout.py
@@ -0,0 +1,36 @@
+#! /usr/bin/env python3
+
+import cookies
+import sqlite3
+
+def html_header():
+  print("""Content-Type: text/html\r\n
+  <!DOCTYPE html>
+  <html>""")
+
+def html_tail():
+  print("""
+  </html>
+  """)
+
+cookie = cookies.create_cookies()
+session_id = cookies.retrieve_cookies(cookie)
+
+if session_id == False:
+  html_header()
+  print("""<meta http-equiv="refresh" content="0; url=/cgi-bin/index.py"/>""")
+  html_tail()
+else:
+    cookies.del_cookies(cookie)
+    conn = sqlite3.connect('../index.db')
+    cursor = conn.cursor()
+    cookies.del_session_id(session_id, cursor, conn)
+    conn.close()
+
+    cookies.cookies_head(cookie)
+    html_header()
+    print("""<h1>Success</h1>
+            <p>Logout success</p>
+            <p>Will be redirected to index page in 2 seconds</p>
+            <meta http-equiv="refresh" content="2; url=/cgi-bin/index.py"/>""")
+    html_tail()
